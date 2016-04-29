5ebf6d1ce11d23e313087f4817f682d212db5964
Omar Padron <omar.padron@kitware.com>
2016-04-29 16:13:18 -0400

    add update bot script

---
+++ scripts/update.js
@@ -0,0 +1,37 @@
+
+"use strict";
+
+let child_process = require("child_process");
+
+module.exports = (robot) => {
+  let currentRevision = child_process.spawnSync(
+    "git",
+    [ "rev-parse", "--no-flags", "HEAD"],
+    { "stdio": ["ignore", "pipe", "ignore"] }
+  ).stdout.toString();
+
+  robot.respond(/update/i, (res) => {
+    child_process.spawnSync(
+      "git", ["fetch", "--all"], { "stdio": "ignore" }
+    );
+    child_process.spawnSync(
+      "git", ["pull"], { "stdio": "ignore" }
+    );
+
+    let newRevision = child_process.spawnSync(
+      "git",
+      [ "rev-parse", "--no-flags", "HEAD"],
+      { "stdio": ["ignore", "pipe", "ignore"] }
+    ).stdout.toString();
+
+    if(newRevision == currentRevision) {
+      res.reply(`already at latest revision: ${currentRevision}`);
+    } else {
+      res.reply(`upgrading from ${currentRevision} to ${newRevision}`);
+      robot.shutdown();
+
+      process.exit(0);
+    }
+  });
+}
+
